10acae2814944ed5c6c192fb4023356fbef5582e
Nagavel <nagavel2010376@ssn.edu.in>
2023-08-16 22:32:08 +0530

    started login page

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,7 +1,43 @@
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+import Form from "components/Form";
 import React from "react";
 
 const LoginPage = () => {
-    return (<div>Login Page</div>)
-}
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+  return (
+    <Box>
+      <Box
+        width={"100%"}
+        bgcolor={theme.palette.background.alt}
+        p={"1rem 6%"}
+        textAlign={"center"}
+      >
+        <Typography
+          fontWeight="bold"
+          fontSize={{ xs: "1rem", sm: "2rem" }}
+          color="primary"
+        >
+          Whisper
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? "50%" : "90%"}
+        p={"2rem"}
+        m={"2rem auto"}
+        borderRadius={"1.5rem"}
+        bgcolor={theme.palette.background.alt}
+      >
+        <Typography fontWeight={"400"} variant="h4" sx={{marginBottom: "1.5rem"}} textAlign={"center"}>
+            Welcome to Whisper, the peaceful and less dramatic social media 
+        </Typography>
+
+        <Form />
+      </Box>
+    </Box>
+  );
+};
 
 export default LoginPage;
