aceeaf00a5309deb43bd059b38e5e912bb606e2f
Nagavel <nagavel2010376@ssn.edu.in>
2023-08-22 20:17:23 +0530

    Finished User Card

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "scenes/navbar";
 import { Box } from "@mui/material";
+import UserWidget from "../widgets/UserWidget";
+import FlexBetween from "components/FlexBetween";
 const HomePage = () => {
   return (
     <Box>
       <Navbar />
+      <FlexBetween>
+        <UserWidget userId={"64cc862adaa829b187ad4b23"} picturePath={"kawaii-cat-animal-cartoon-design-free-vector.jpg"}/>
+      </FlexBetween>
     </Box>
   );
 };
